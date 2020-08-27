4c7c2484c2cd233d6d22a752f427bc70ed4565d2
Pharoah Campbell <pharoahcampbell@gmail.com>
2020-08-27 13:49:18 -0400

    Added nav bar

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
+import HomePage from "./HomePage";
+import ErrorPage from "./ErrorPage";
+import NavigationBar from "./NavigationBar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <NavigationBar />
+        <Router>
+          <Switch>
+            <Route path = "/" component = {HomePage}/>
+            <Route component = {ErrorPage} />
+          </Switch>
+        </Router>
+      </React.Fragment>
   );
 }
 
